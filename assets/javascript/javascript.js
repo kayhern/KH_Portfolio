3d1f9fb6c16f2d854dd2f994d484fae855d6d96b
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-11-15 20:46:50 -0500

    changes

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,15 @@
+$("#contact").click(function () {
+    window.open("mailto:kristen.e.hernandez@gmail.com");
+});
+
+$("#github").click(function () {
+    window.location = "https://github.com/kayhern";
+});
+
+$("#linkedin").click(function () {
+    window.location = "https://www.linkedin.com/in/kristen-hernandez-30593452/";
+});
+
+$("#resume").click(function () {
+    window.location = "../images/khernandezresume.pdf";
+});
