f5a8083b10eda815c116b7381b45ca6737b505e1
Kristen Hernandez <kristen.e.hernandez@gmail.com>
2019-11-15 21:21:21 -0500

    links

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -9,3 +9,19 @@ $("#linkedin").click(function () {
 $("#resume").click(function () {
     window.location = "https://github.com/kayhern/KH_Portfolio/raw/master/assets/images/khernandezresume.pdf";
 });
+
+$("#projects").click(function () {
+    window.location = "project.html";
+});
+
+$("#Trivia-Game").click(function () {
+    window.location = "https://kayhern.github.io/TriviaGame/";
+});
+
+$("#GifTastic").click(function () {
+    window.location = "https://kayhern.github.io/GifTastic/";
+});
+
+$("#JewelGame").click(function () {
+    window.location = "https://kayhern.github.io/unit-4-game/";
+});
